c9c5767ab5f69a9f43a634cee0672b23f12a9700
Sam <savaka2@users.noreply.github.com>
2020-02-19 22:40:59 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const version = '[incomplete]';
+
+const uiAnimTime = 250;
+const animTime = 500; // just toasts here
+
+// update notes, oldest first
+// text is actually html
+const updateNotes = [];
---
+++ js/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// begin pwa install stuff
+
+let installPrompt = null;
+
+window.addEventListener('beforeinstallprompt', function(e) {
+	installPrompt = e;
+	$('#installButton').parent().show();
+});
+
+window.addEventListener('appinstalled', function(e) {
+	hideInstallOption(true);
+});
+
+function hideInstallOption(installed) {
+	if (installed) {
+		$('#installButton').parent().hide();
+	}
+	if ($('#installModal:visible').length > 0) {
+		closeModal(function() {hideInstallOption2(installed);});
+	} else {
+		hideInstallOption2(installed);
+	}
+}
+
+function hideInstallOption2(installed) { // the part that may or may not occur after closing the modal
+	$('#installConfirmButton').hide();
+	if (!installed) {$('#installModal p').html('Please <a href="index.html">reload the page</a> to install the web app.');}
+}
+
+// end pwa install stuff
