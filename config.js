41b7336c192c2c2ec5ac4b8f83458b62a45561c4
Sam <savaka2@users.noreply.github.com>
2020-02-20 12:48:03 -0500

    add some stuff

--- config.js
+++ config.js
@@ -1,10 +1,4 @@
 'use strict';
 
-const version = '[incomplete]';
-
 const uiAnimTime = 250;
 const animTime = 500; // just toasts here
-
-// update notes, oldest first
-// text is actually html
-const updateNotes = [];
--- js/index.js
+++ js/index.js
@@ -30,3 +30,54 @@ function hideInstallOption2(installed) { // the part that may or may not occur a
 }
 
 // end pwa install stuff
+
+$(document).ready(function() {
+	// add events
+	// TODO: android back button
+	// to scroll to top by clicking the logo
+	$('#logoLink').on('click', function(e) {
+		if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+			$('html, body').animate({scrollTop: 0}, uiAnimTime);
+		} else {
+			$('html, body').scrollTop(0);
+		}
+	});
+	// to open the install modal
+	$('#installButton').on('click', function(e) {
+		openModal('#installModal');
+	});
+	// to show the install prompt (pwa)
+	$('#installConfirmButton').on('click', function(e) {
+		installPrompt.prompt();
+		hideInstallOption(false);
+		installPrompt.userChoice.then(function(choice) {
+			if (choice.outcome == 'accepted') {hideInstallOption(true);}
+			installPrompt = null;
+		});
+	});
+	// to open the menu modal
+	$('#menuButton').on('click', function(e) {
+		showIftttKey();
+		openModal('#menuModal');
+	});
+	// to change the ifttt key
+	$('#setIftttKeyButton').on('click', function(e) {
+		let newKey = prompt('Paste your IFTTT Webhooks key');
+		if (newKey != null) {
+			lsSet('iftttKey', newKey);
+			showIftttKey();
+		}
+	});
+	
+	$('#menuButton').attr('disabled', false);
+	$('#viewsButton').attr('disabled', false);
+});
+
+function showIftttKey() {
+	let key = lsGet('iftttKey', '');
+	if (key == '') {
+		$('#iftttKey').html('<i>Not set</i>');
+	} else {
+		$('#iftttKey').text(lsGet('iftttKey', ''));
+	}
+}
